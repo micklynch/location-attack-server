9b6a4d5b688aa78eaceeb427ed5e7cf42d7799cd
micklynch <mick.lynch@gmail.com>
2020-10-06 11:16:11 -0400

    Clean up for Medium article

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import firebase, { database } from "firebase/app";
-import crypto, { async } from "crypto-random-string";
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import bodyParser from "body-parser";
@@ -10,7 +9,7 @@ import { FirebaseFunctionsRateLimiter } from "firebase-functions-rate-limiter";
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-// Limit the number of hits on my API
+// Limit the number of requests to my server. This is here to stop my firebase account from getting charged
 const limiterConfiguration = {
   name: "rate_limiter_collection", // a collection with this name will be created
   periodSeconds: 15, // the length of test period in seconds
@@ -21,7 +20,7 @@ const limiter = FirebaseFunctionsRateLimiter.withFirestoreBackend(
   limiterConfiguration,
   db
 );
-
+// User details expected from the client
 type UserDetails = {
   username: string;
   password: string;
@@ -32,89 +31,113 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
+/**
+ * Home route
+ */
 app.get("/", async (req, res) => {
   await limiter.rejectOnQuotaExceededOrRecordUsage(); // will throw HttpsException with proper warning
-  res.send(req.ip);
+  res.status(200).send({
+    Hello: "World",
+  });
 });
-
+/**
+ * Login route
+ * Takes a user crendials object as input
+ * @param {username: "test@email.com", password : "sTrOnGpAsSwOrD"}
+ *
+ */
 app.post("/login", async (req, res) => {
   await limiter.rejectOnQuotaExceededOrRecordUsage(); // will throw HttpsException with proper warnin
   var ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
-  const userDetails: UserDetails = req.body;
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(userDetails.username, userDetails.password)
-    .then(function (userObj) {
-      isNewLocation(userObj.user.uid, ip)
-        .then(function (isNew) {
-          if (isNew) {
-            const token = crypto({ length: 32 });
-            storeTokenForUser(userObj.user.uid, ip, token)
-              .then(function () {
-                emailLinkToUse(userObj.user.email, token).then(function () {
-                  res.statusMessage = token;
-                  res.status(400).end();
-                });
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-          } else {
-            res.status(200).send(userObj.user.email);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
+  const userDetails = req.body as UserDetails;
+  try {
+    const userObj = await firebase
+      .auth()
+      .signInWithEmailAndPassword(userDetails.username, userDetails.password);
+    if (userObj) {
+      // check if this is a new location for the user
+      const newLocation = await isNewLocation(userObj.user.uid, ip);
+      // if new location, store the new location and
+      if (newLocation) {
+        // generate a random token
+        const token = CreateUUID();
+        await storeTokenForUser(userObj.user.uid, ip, token);
+        emailLinkToUser(userObj.user.email, token);
+        res.status(202).send({
+          message: "You will receive an email with a link to login",
         });
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(404).send({ error: "User not found" });
+      } else {
+        // Send whatever information you want back to the client (in my case, just the email)
+        res.status(200).send(userObj.user.email);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message:
+        "There was an error logging in, please check your username and password",
     });
+  }
 });
-
+/**
+ * Verify route
+ * @param token
+ */
 app.get("/verifyLocation", async (req, res) => {
+  await limiter.rejectOnQuotaExceededOrRecordUsage(); // will throw HttpsException with proper warnin
   const token = req.query.token;
   let userId = "";
   let newIpAddress = "";
-  const querySnapshot = await db
-    .collection("userLocations")
-    .where("token", "==", token)
-    .get();
-  if (querySnapshot.empty) {
-    console.log("Error");
-    res.status(400).send({ error: "Link no longer valid" });
-  }
+  try {
+    const querySnapshot = await db
+      .collection("userLocations")
+      .where("token", "==", token)
+      .get();
+    if (querySnapshot.empty) {
+      console.log("Error");
+      res.status(204).send({ error: "Link no longer valid" });
+    }
 
-  querySnapshot.forEach((doc) => {
-    userId = doc.id;
-    newIpAddress = doc.data().tokenLocation;
-    const docRef = db.collection("userLocations").doc(userId);
-    // Get a new write batch
-    var batch = db.batch();
-    batch.update(docRef, {
-      locationHistory: firebase.firestore.FieldValue.arrayUnion(newIpAddress),
+    querySnapshot.forEach((doc) => {
+      userId = doc.id;
+      newIpAddress = doc.data().tokenLocation;
+      const docRef = db.collection("userLocations").doc(userId);
+      // Get a new write batch
+      var batch = db.batch();
+      batch.update(docRef, {
+        locationHistory: firebase.firestore.FieldValue.arrayUnion(newIpAddress),
+      });
+      batch.set(
+        docRef,
+        {
+          token: firebase.firestore.FieldValue.delete(),
+          tokenLocation: firebase.firestore.FieldValue.delete(),
+        },
+        { merge: true }
+      );
+      batch.commit();
+      res.status(200).send({
+        message: "success",
+      });
     });
-    batch.set(
-      docRef,
-      {
-        token: firebase.firestore.FieldValue.delete(),
-        tokenLocation: firebase.firestore.FieldValue.delete(),
-      },
-      { merge: true }
-    );
-    let result = batch.commit();
-    res.status(200).send({
-      message: "success",
-    });
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
+/**
+ * Server up and running!
+ */
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
+/**
+ * Checks if this location is new for this user
+ * @param userId Firebase UUID for the user
+ * @param ipLocation Location that the user is logging in from
+ */
 const isNewLocation = async function (
   userId: string,
   ipLocation: string
@@ -129,7 +152,12 @@ const isNewLocation = async function (
   }
   return true;
 };
-
+/**
+ *
+ * @param userId Firebase UUID for the user
+ * @param newIpAddress ip address that they are trying to log in from
+ * @param token generated token that is associated with the new location
+ */
 const storeTokenForUser = async function (
   userId: string,
   newIpAddress: string,
@@ -146,11 +174,27 @@ const storeTokenForUser = async function (
   return;
 };
 
-const emailLinkToUse = async function (userEmail, token) {
+/**
+ *
+ * @param userEmail user's email address
+ * @param token generated token associated with the new location
+ */
+const emailLinkToUser = function (userEmail: string, token: string): boolean {
   //
   // Send an email to the user with the token in the format:
   // https://yourserveraddress.com/?token={token}
   //
   console.log(`Sending email to user ${userEmail} with token=${token}`);
   return true;
+};
+
+/**
+ * Generate a UUID
+ */
+const CreateUUID = function (): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 };
